954cb882f89a3e40600bb30aeb5ba588d9120228
Joshua Quaintance <45566099+JoshQuaintance@users.noreply.github.com>
2023-01-11 20:34:22 -0500

    Creating config file for jekyll

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 from os import makedirs, rmdir
 import time
+import yaml
+
 
 from cleandir import cleandir
 from convert import convert
 from fixlinks import fixlinks
-from utils import resolve_path
+from utils import resolve_path, read_file
 
 
 def main():
+    out = '../odp-portal-test'
     start_time = time.time()
-    # print(resolve_path('./out'))
 
     convert_time = time.time()
-    convert('./example', './out')
+    convert('./example', out)
     convert_time = time.time() - convert_time
 
     cleandir_time = time.time()
-    cleandir('./out')
+    cleandir(out)
     cleandir_time = time.time() - cleandir_time
 
     fixlinks_time = time.time()
-    fixlinks('./out')
+    fixlinks(out)
     fixlinks_time = time.time() - fixlinks_time
 
     print(f'Scripts took {time.time() - start_time:.2f} seconds to run...')
@@ -31,5 +33,35 @@ def main():
     print(f'Link Fixing time: {fixlinks_time:.2f}s')
 
 
+def setup_config(ROOT_DIR):
+    ROOT_DIR = resolve_path(ROOT_DIR, True)
+
+    config = read_file(ROOT_DIR + '_config.yml')
+
+    try:
+        parsed = yaml.safe_load(config)
+        old_collections = parsed['collections']
+
+        # TODO: Write actual code that will output a correct config file
+        new_collections = {
+            'ODP': {
+                'output': True
+            }
+        }
+
+        # Dictionary union https://peps.python.org/pep-0584/
+        parsed['collections'] = new_collections | old_collections
+
+        updated_config = yaml.dump(parsed)
+
+        header = "This is a generated config. Do not mess with it unless you know what you are doing!\n\n"
+
+        with open(ROOT_DIR + '_config.yml', 'w') as config_f:
+            config_f.write(header + updated_config)
+
+    except yaml.YAMLError as err:
+        print(err)
+
+
 if __name__ == '__main__':
-    main()
+    setup_config('../odp-portal-test')
